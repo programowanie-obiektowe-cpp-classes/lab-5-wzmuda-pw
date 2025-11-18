015d6b0cd8c77787b23b7eafb06a0f85a449b45d
wtzmuda <wtzmuda@gmail.com>
2025-11-19 00:10:53 +0100

    zejsciowka 5 v1
    Implement foo function to process Human objects and return a vector of characters based on their monster status.

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,29 @@
 #pragma once
-
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
+#include <algorithm> 
+#include <print>     
 
-std::vector< char > foo(std::list< Human >& people)
+inline std::vector<char> foo(std::list<Human>& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector<char> result;
+    
+    result.reserve(people.size());
+
+    for (auto& human : people) {
+        
+        human.birthday();
+
+        if (human.isMonster()) {
+            result.push_back('n');
+        } else {
+            result.push_back('y');
+        }
+
+        std::print("Zaktualizowano Human. isMonster: {}\n", human.isMonster());
+    }
+    std::reverse(result.begin(), result.end());
+
+    return result;
 }
